391f3f8e374b3d653869197698eb78925e0fc647
garcia-laura <jhony@Air-de-Jhony.home>
2024-07-04 00:53:35 +0200

    app end

--- src/app/components/convertidor/convertidor.component.ts
+++ src/app/components/convertidor/convertidor.component.ts
@@ -10,5 +10,46 @@ cantidad = 0;
 tengo = 'USD';
 quiero = 'Euro'
 total= 0;
-monedas: string[] = ['USD', 'LIBRA', 'EURO']
+monedas: string[] = ['USD', 'LIBRA', 'EURO'];
+
+convertir() {
+  console.log('metdodong')
+  switch (this.tengo) {
+    case 'USD':
+      if(this.quiero === 'USD')(
+        this.total = this.cantidad
+      )
+      if(this.quiero === 'LIBRA')(
+        this.total = this.cantidad * 0.78
+      ) 
+      if(this.quiero === 'EURO')(
+        this.total = this.cantidad * 0.93
+      ) 
+      break
+      case 'LIBRA':
+        if(this.quiero === 'LIBRA')(
+          this.total = this.cantidad
+        )
+        if(this.quiero === 'USD')(
+          this.total = this.cantidad * 1,27
+        ) 
+        if(this.quiero === 'EURO')(
+          this.total = this.cantidad * 1.18
+        ) 
+      break
+      case 'EURO':
+        if(this.quiero === 'EURO')(
+          this.total = this.cantidad
+        )
+        if(this.quiero === 'USD')(
+          this.total = this.cantidad * 1.08
+        ) 
+        if(this.quiero === 'LIBRA')(
+          this.total = this.cantidad * 0.85
+        ) 
+      break
+  }
+  
 }
+}
+
